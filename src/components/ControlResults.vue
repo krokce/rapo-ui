b6cfe6a79409cc521d7bdcf04974871c81f374fd
Kostadin Taneski <kosta@taneski.com>
2023-12-30 15:18:47 +0100

    Redesign control results page

--- src/components/ControlResults.vue
+++ src/components/ControlResults.vue
@@ -1,39 +1,86 @@
 <template>
   <q-page>
-    <h1>
-      {{ filteredControlResultsLen }} Control result<span
-        v-if="filteredControlResultsLen != 1"
-        >s</span
-      >
-    </h1>
+    <h1>{{ filteredControlResultsLen }} Control result<span v-if="filteredControlResultsLen != 1">s</span></h1>
 
     <div class="q-pa-md">
-      <q-table
-        class="my-sticky-header-table"
-        flat
-        bordered
-        dense
-        :rows="filteredControlResults"
-        :columns="resColumns"
-        row-key="process_id"
-        virtual-scroll
-        :rows-per-page-options="[0]"
-        v-model:pagination="pagination">
-      </q-table>
+      <q-markup-table dense="true">
+        <thead>
+          <tr class="bg-blue-grey-2">
+            <th class="text-left">Run timestamp</th>
+            <th class="text-center">Run duration</th>
+            <th class="text-left">PID</th>
+            <th class="text-left">Processname</th>
+            <th class="text-left">Run from</th>
+            <th class="text-left">Run to</th>
+            <th class="text-right">Fetched A</th>
+            <th class="text-right">Fetched B</th>
+            <th class="text-right">Discr. A</th>
+            <th class="text-right">Discr. B</th>
+            <th class="text-right">Err. lvl A [%]</th>
+            <th class="text-right">Err. lvl B [%]</th>
+            <th class="text-right">Prerequisite</th>
+            <th class="text-left">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="control in filteredControlResults" :key="control.process_id">
+            <td class="text-left">
+              <strong>{{ toDateTimeString(control.start_date) }}</strong>
+            </td>
+            <td class="text-center">{{ round(control.duration_minutes, 2) }} min</td>
+            <td class="text-left text-weight-bold text-blue-grey-7">{{ control.process_id }}</td>
+            <td class="text-left text-weight-bold text-teal">
+              <router-link
+                :to="{
+                  name: 'edit-control',
+                  params: { controlId: control.control_id },
+                }">
+                {{ control.control_name }}
+              </router-link>
+            </td>
+            <td class="text-left">{{ toDateString(control.date_from) }}</td>
+            <td class="text-left">{{ toDateString(control.date_to) }}</td>
+            <td class="text-right">{{ control.fetched_a }}</td>
+            <td class="text-right">{{ control.fetched_b }}</td>
+            <td class="text-right">{{ control.errors_a }}</td>
+            <td class="text-right">{{ control.errors_b }}</td>
+            <td class="text-right text-red">{{ round(control.error_level_a, 2) }}%</td>
+            <td class="text-right text-red">{{ round(control.error_level_b, 2) }}%</td>
+            <td class="text-right">{{ control.prerequisite_value }}</td>
+            <td class="text-left">
+              <q-chip class="cursor-pointer">
+                <q-avatar
+                  v-if="control.status == 'Error'"
+                  icon="fas fa-exclamation-circle"
+                  color="deep-orange"
+                  text-color="white"
+                  @click="showErrorDialog(control)" />
+                <q-avatar v-if="control.status == 'Success'" icon="fas fa-check-circle" color="green" text-color="white" />
+                <q-avatar v-if="control.status == 'Initiated'" icon="fas fa-play-circle" color="blue" text-color="white" />
+                <q-avatar v-if="control.status == 'Deleted'" icon="fas fa-times-circle" color="grey" text-color="white" />
+                <q-avatar v-if="control.status == 'Running'" icon="fas fa-sync fa-spin" color="blue" text-color="white" />
+                {{ control.status }}
+              </q-chip>
+            </td>
+          </tr>
+        </tbody>
+      </q-markup-table>
     </div>
-
   </q-page>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
+import { useQuasar } from "quasar";
 
 export default {
   data() {
     return {
+      $q: useQuasar(),
       pagination: {
         rowsPerPage: 0,
       },
+      refreshTimer: null,
     };
   },
   methods: {
@@ -43,8 +90,23 @@ export default {
       return ret;
     },
     toDateTimeString(val) {
-      var ret = new Date(val).toISOString("de-DE").substring(0, 10);
+      var ret = new Date(val).toISOString("de-DE").substring(0, 19).replace("T", " ");
       return ret;
+    },
+    round(val, places) {
+      return Math.round(val * Math.pow(10, places)) / Math.pow(10, places);
+    },
+    showErrorDialog(control) {
+      this.$q.dialog({
+        title: control.control_name,
+        message: control.text_error,
+      });
+    },
+    startRefreshTimer() {
+      this.refreshTimer = setInterval(this.updateControlResults, 10000);
+    },
+    stopRefreshTimer() {
+      clearInterval(this.refreshTimer);
     },
   },
   computed: {
@@ -52,176 +114,29 @@ export default {
     filteredControlResultsLen() {
       return this.filteredControlResults.length;
     },
-    resColumns() {
-      return [
-        {
-          name: "process_id",
-          align: "left",
-          label: "PROCESS ID",
-          field: "process_id",
-          classes: "text-weight-bold text-blue-grey-7",
-          sortable: true,
-        },
-        {
-          name: "start_date",
-          align: "right",
-          label: "START TIMSTAMP",
-          field: "start_date",
-          format: (val) =>
-            new Date(val)
-              .toISOString("de-DE")
-              .substring(0, 19)
-              .replace("T", " "),
-          classes: "text-weight-bold",
-          sortable: true,
-        },
-        {
-          name: "date_from",
-          align: "right",
-          label: "FROM",
-          field: "date_from",
-          format: (val) =>
-            new Date(val)
-              .toISOString("de-DE")
-              .substring(0, 10)
-              .replace("T", " "),
-        },
-        {
-          name: "date_to",
-          align: "right",
-          label: "TO",
-          field: "date_to",
-          format: (val) =>
-            new Date(val)
-              .toISOString("de-DE")
-              .substring(0, 10)
-              .replace("T", " "),
-        },
-        {
-          name: "duration_minutes",
-          align: "right",
-          label: "DURATION",
-          field: "duration_minutes",
-          format: (val) => `${val} min.`,
-        },
-        {
-          name: "control_name",
-          align: "left",
-          label: "CONTROL NAME",
-          field: "control_name",
-          classes: "text-weight-bold text-teal",
-          sortable: false,
-        },
-        {
-          name: "fetched_a",
-          align: "right",
-          label: "FETCHED A",
-          field: "fetched_a",
-          sortable: false,
-        },
-        {
-          name: "fetched_b",
-          align: "right",
-          label: "FETCHED B",
-          field: "fetched_b",
-          sortable: false,
-        },
-        {
-          name: "errors_a",
-          align: "right",
-          label: "ERRORS A",
-          field: "errors_a",
-          sortable: false,
-        },
-        {
-          name: "errors_b",
-          align: "right",
-          label: "ERRORS B",
-          field: "errors_b",
-          sortable: false,
-        },
-        {
-          name: "error_level",
-          align: "right",
-          label: "ERRORS [%]",
-          field: "error_level_a",
-          format: (val) => `${val}%`,
-          classes: "text-red",
-          sortable: false,
-        },
-        {
-          name: "status",
-          align: "center",
-          label: "STATUS",
-          field: "status",
-          sortable: true,
-        },
-        {
-          name: "prerequisite_value",
-          align: "right",
-          label: "PREREQUISITE",
-          field: "prerequisite_value",
-          sortable: false,
-        },
-        {
-          name: "error",
-          align: "left",
-          label: "ERROR",
-          field: "text_error",
-          sortable: true,
-        },
-      ];
-    },
-    resRows() {
-      let rows = [];
-      for (let i = 0; i < this.filteredControlResults.length; i++) {
-        var ctrl = this.filteredControlResults[i];
-        rows.push({
-          process_id: ctrl.process_id,
-          control_name: ctrl.control_name,
-          start_date: ctrl.start_date,
-          date_from: ctrl.date_from,
-          date_to: ctrl.date_to,
-          error_level: ctrl.error_level ? ctrl.error_level : "0",
-          status: ctrl.status,
-        });
-      }
-      rows.forEach((row, index) => {
-        row.index = index;
-      });
-      return rows;
-    },
   },
   mounted() {
     this.updateControlResults();
+    this.startRefreshTimer();
+  },
+  unmounted() {
+    this.stopRefreshTimer();
   },
 };
 </script>
 
-<style lang="sass">
-.my-sticky-header-table
-  /* height or max-height is important */
-  height: 550px
+<style lang="css" scoped>
+/* Override the default link styles */
+a {
+  color: #009688;
+  text-decoration: none;
+}
 
-  .q-table__top,
-  .q-table__bottom,
-  thead tr:first-child th
-    /* bg color is important for th; just specify one */
-    background-color: #ccc
+a:hover {
+  text-decoration: underline;
+}
 
-  thead tr th
-    position: sticky
-    z-index: 1
-  thead tr:first-child th
-    top: 0
-
-  /* this is when the loading indicator appears */
-  &.q-table--loading thead tr:last-child th
-    /* height of all previous header rows */
-    top: 48px
-
-  /* prevent scrolling behind sticky top row on focus */
-  tbody
-    /* height of all previous header rows */
-    scroll-margin-top: 48px
+a:visited {
+  color: #009688;
+}
 </style>
