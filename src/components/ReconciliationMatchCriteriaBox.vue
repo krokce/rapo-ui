c6beafac2285d612d89e6a808a128a3c2b04c83f
Kostadin Taneski <kosta@taneski.com>
2026-01-16 09:39:06 +0100

    Add "Formula mode" in match criteria

--- src/components/ReconciliationMatchCriteriaBox.vue
+++ src/components/ReconciliationMatchCriteriaBox.vue
@@ -8,6 +8,7 @@
       <q-card-section class="q-gutter-xs">
         <div class="row q-gutter-xs items-center" v-for="(item, index) in ruleConfigObject.correlation_config" v-bind:key="index">
           <q-select
+            v-if="!ruleConfigObject.correlation_config[index].formula_mode"
             use-input
             hide-selected
             fill-input
@@ -18,9 +19,22 @@
             :options="datasourceAList"
             label="Field A" />
 
+          <q-input
+            v-if="ruleConfigObject.correlation_config[index].formula_mode"
+            outlined
+            class="col-2"
+            v-model="ruleConfigObject.correlation_config[index].field_a"
+            label="Field A">
+            <template v-slot:append>
+              <q-icon name="fas fa-calculator" @click.stop.prevent />
+            </template>
+            <q-tooltip anchor="top left" self="bottom left" :offset="[0, 5]"> Aways use 'a.' as prefix to DB fields in formula mode </q-tooltip>
+          </q-input>
+
           <q-icon name="fas fa-equals" size="20px" color="blue-grey-3" />
 
           <q-select
+            v-if="!ruleConfigObject.correlation_config[index].formula_mode"
             use-input
             hide-selected
             fill-input
@@ -30,6 +44,18 @@
             v-model="ruleConfigObject.correlation_config[index].field_b"
             :options="datasourceBList"
             label="Field B" />
+
+          <q-input
+            v-if="ruleConfigObject.correlation_config[index].formula_mode"
+            outlined
+            class="col-2"
+            v-model="ruleConfigObject.correlation_config[index].field_b"
+            label="Field B">
+            <template v-slot:append>
+              <q-icon name="fas fa-calculator" @click.stop.prevent />
+            </template>
+            <q-tooltip anchor="top left" self="bottom left" :offset="[0, 5]"> Aways use 'b.' as prefix to DB fields in formula mode </q-tooltip>
+          </q-input>
 
           <q-icon name="fas fa-circle" size="15px" color="blue-grey-3" />
 
@@ -48,6 +74,49 @@
               If set to "Yes" records with null keys will be included in the reconciliation and marked as discrepancies
             </q-tooltip>
           </q-select>
+
+          <q-icon name="fas fa-circle" size="15px" color="blue-grey-3" />
+
+          <q-select
+            class="col-1"
+            outlined
+            emit-value
+            map-options
+            v-model="ruleConfigObject.correlation_config[index].formula_mode"
+            :options="[
+              { label: 'Yes', value: true },
+              { label: 'No', value: false },
+            ]"
+            label="Formula"
+            @update:model-value="
+              (value) => {
+                ruleConfigObject.correlation_config[index].formula_mode = value;
+                if (value) {
+                  ruleConfigObject.correlation_config[index].formula_alias = ruleConfigObject.correlation_config[index].field_a;
+                  ruleConfigObject.correlation_config[index].field_a = 'a.' + ruleConfigObject.correlation_config[index].field_a;
+                  ruleConfigObject.correlation_config[index].field_b = 'b.' + ruleConfigObject.correlation_config[index].field_b;
+                } else {
+                  ruleConfigObject.correlation_config[index].field_a = datasourceAColumns[0];
+                  ruleConfigObject.correlation_config[index].field_b = datasourceBColumns[0];
+                }
+              }
+            ">
+            <q-tooltip anchor="top left" self="bottom left" :offset="[0, 5]">
+              In "Formula mode" you can use expressions instead of field names, e.g. "a.AMOUNT + a.TAX"
+            </q-tooltip>
+          </q-select>
+          <q-input
+            v-if="ruleConfigObject.correlation_config[index].formula_mode"
+            outlined
+            class="col-2"
+            v-model="ruleConfigObject.correlation_config[index].formula_alias"
+            label="Formula alias">
+            <template v-slot:prepend>
+              <q-icon name="fas fa-font" @click.stop.prevent />
+            </template>
+            <q-tooltip anchor="top left" self="bottom left" :offset="[0, 5]"> Name to be used when presenting the discrepancies </q-tooltip>
+          </q-input>
+
           <q-btn size="sm" color="primary" flat round icon="fas fa-minus" @click="removeCorrelationConfig(index)" />
           <q-btn
             v-if="index == ruleConfigObject.correlation_config.length - 1"
